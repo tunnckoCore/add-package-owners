56f48fa635247ba308d4580a153788a86b30944e
tunnckoCore <mameto_100@mail.bg>
2015-04-03 19:27:16 +0300

    0.1.0

---
+++ cli.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+/**
+ * add-package-owners <https://github.com/tunnckoCore/add-package-owners>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+'use strict';
+
+var fs = require('fs');
+var got = require('got');
+var cmd = require('spawn-commands');
+var link = process.argv.slice(2)[0];
+var url = link || 'https://cdn.rawgit.com/jstransformers/meta/master/owners.json';
+
+got.get(url, function(err, res) {
+  var owners = JSON.parse(res).owners;
+  var cmds = [];
+  delete owners[0];
+
+  owners.filter(Boolean).forEach(function(name) {
+    cmds.push({cmd: 'npm', args: ['owner', 'add', name]});
+  });
+  cmd(cmds, function(err) {
+    console.log(err ? err : 'complete');
+  });
+});
+
